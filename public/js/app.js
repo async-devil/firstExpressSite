6f8f90610cdae3f53fa824f62de336e76c400d3c
async-devil <async.devil@gmail.com>
2020-08-23 21:41:11 +0300

    Added GUI to API

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,23 @@
-fetch('weather/api?search=London').then((res) => {
-  res.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else if (data.error === 'Time API 404') {
-      console.log(data.weatherData)
-    } else {
-      console.log(data.weatherData)
-      console.log(data.timeData);
-    }
-  })
+const form = document.querySelector('form')
+const search = document.querySelector('input')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  console.log(search.value);
+  if (search.value !== '') {
+    fetch('http://localhost:3000/app/weather/api?search=' + search.value).then((res) => {
+      res.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+        } else if (data.error === 'Time API 404') {
+          console.log(data.weatherData)
+        } else {
+          console.log(data.weatherData)
+          console.log(data.timeData);
+        }
+      })
+    })
+  } else {
+    alert('Please input something')
+  }
 })
