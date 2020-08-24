c3bf7dee63cec51995e8949e7f3e3086c34459dd
async-devil <async.devil@gmail.com>
2020-08-24 11:21:32 +0300

    Added heroku support

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const publicDirPath = path.join(__dirname, "../public");
 const partialsDirPath = path.join(__dirname, "../templates/partials");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "hbs");
 app.set("views", viewsDirPath);
@@ -112,6 +113,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is on port 3000");
+app.listen(port, () => {
+  console.log("Server is on port " + port);
 });
